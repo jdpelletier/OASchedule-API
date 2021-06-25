8dc724e9aa29f519c62b4bc9ed911265b4fb720b
jdpelletier <john.pelletier@ymail.com>
2021-06-24 21:25:37 -1000

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .Util import writeToJson, readFromJson
 from flask_cors import cross_origin
 
@@ -8,9 +8,10 @@ main = Blueprint('main', __name__)
 @main.route('/update_schedule', methods=['POST'])
 @cross_origin()
 def update_schedule():
-    f = 'C:\\Users\\johnp\\Desktop\\Keck\\Software\\OASchedule\\OASchedule-API\\sched.xlsx'
+    f = request.files['file']
+    # f = 'C:\\Users\\johnp\\Desktop\\Keck\\Software\\OASchedule\\OASchedule-API\\sched.xlsx'
     status = writeToJson(f)
-    return 'Done', status
+    return readFromJson('data.json')
 
 @main.route('/')
 @cross_origin()
