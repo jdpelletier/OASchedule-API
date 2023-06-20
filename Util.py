d290f818df3ee4df66457b2f3161d827a354ae30
jdpelletier <john.pelletier@ymail.com>
2023-06-20 10:50:45 -1000

    Update Util.py

--- Util.py
+++ Util.py
@@ -118,7 +118,6 @@ def readFromTelSched():
             # night["Holiday"] = None #todo get holidays
             response = requests.get(f"https://www.keck.hawaii.edu/software/db_api/telSchedule.php?cmd=getSchedule&date={d}&column=Date,Instrument,Institution,TelNr,Principal")
             observers = response.json()
-            print(observers)
             os = []
             for x in range(0, len(observers)):
                 os += observers[x]
