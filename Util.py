5614aef48ae6d7b541322dd819a17acfe54d573d
jdpelletier <john.pelletier@ymail.com>
2023-06-19 14:54:28 -1000

    Update Util.py

--- Util.py
+++ Util.py
@@ -104,7 +104,7 @@ def readFromTelSched():
             night = {}
             night["DOW"] = d.strftime('%A')[:3]
             night["Date"] = datetime.fromtimestamp(time.mktime(d.timetuple())).timestamp()*1000
-            night["Holiday"] = None #todo get holidays
+            # night["Holiday"] = None #todo get holidays
             for name in oa_names:
                 night[name] = None
             for staff in nightstaff:
