1c99d9a64349d9a457ed2208cca9a40e7bb9276b
jdpelletier <john.pelletier@ymail.com>
2023-06-27 10:51:12 -1000

    Update Util.py

--- Util.py
+++ Util.py
@@ -41,7 +41,7 @@ def writeToJson(f):
     df.rename(columns={'K2 PI last':'K2 PI'}, inplace=True)
 
     #TODO get holidays and payperiods working here
-    hol = get_holidays(datetime.strptime(list(df['Date'].values[:1])[0], '%Y-%m-%d').date(), datetime.strptime(list(df['Date'].values[-1:])[0], '%Y-%m-%d').date())
+    hol = get_holidays(list(df['Date'].values[:1])[0], list(df['Date'].values[-1:])[0])
     # hol = get_holidays(datetime.strptime(str(df['Date'].values[:1])[2:12], '%Y-%m-%d').date(), datetime.strptime(str(df['Date'].values[-1:])[2:12], '%Y-%m-%d').date())
     print(hol)
     h_rows = df.loc[df['Date'].isin(hol)]
