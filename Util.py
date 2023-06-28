da8b6e346d807108f2f68c2765aefc4a0179d7ff
jdpelletier <john.pelletier@ymail.com>
2023-06-27 16:19:21 -1000

    Update Util.py

--- Util.py
+++ Util.py
@@ -148,23 +148,25 @@ def getNSFromTelSched(range):
 
         people = 0
         for staff in night:
-            for oa in staff:
-                s_date = datetime.strptime(oa["Date"], '%Y-%m-%d').date()
+            print(staff)
+            s_date = datetime.strptime(staff[0]["Date"], '%Y-%m-%d').date()
                 if s_date > current_date:
                     break
                 if s_date == current_date:
-                    name = oa["FirstName"][0] + oa["LastName"][0]
-                    shift = oa["Type"].upper()
-                    if "R" in shift:
-                        tel = "R" + oa["TelNr"]
-                        night[name] = shift.replace("OAR", tel)
-                        people += 1
-                    else:
-                        tel = "K" + oa["TelNr"]
-                        night[name] = shift.replace("OA", tel)
-                        people += 1
-            if people == 0:
-                break
+                    for oa in staff:
+                        print(oa)
+                        name = oa["FirstName"][0] + oa["LastName"][0]
+                        shift = oa["Type"].upper()
+                        if "R" in shift:
+                            tel = "R" + oa["TelNr"]
+                            night[name] = shift.replace("OAR", tel)
+                            people += 1
+                        else:
+                            tel = "K" + oa["TelNr"]
+                            night[name] = shift.replace("OA", tel)
+                            people += 1
+                    if people == 0:
+                        break
 
         night["K2 PI"] = ""
         night["K2 Institution"] = ""
