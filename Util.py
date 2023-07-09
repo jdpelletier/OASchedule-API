9bf927c16b9e35f1b2984e5c779e374cd4698ed1
jdpelletier <john.pelletier@ymail.com>
2023-07-08 21:06:17 -1000

    Update Util.py

--- Util.py
+++ Util.py
@@ -92,7 +92,7 @@ def compareJsons(excelfile):
         tsnight = ns[i]
         for col in night:
             if night[col] != tsnight[col]:
-                excelfile[night].telschedmismatch = 'X'
+                night['telschedmismatch'] = 'X'
         i+=1
 
     return json.dumps(exce)
