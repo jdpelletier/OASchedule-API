76e63e3b7fc8d19a90c6df22b9ca3b57dc85d8e6
jdpelletier <john.pelletier@ymail.com>
2023-08-11 14:05:37 -1000

    Update Util.py

--- Util.py
+++ Util.py
@@ -5,6 +5,9 @@ import requests
 from dateutil.relativedelta import relativedelta
 from calendar import Calendar
 import time
+import urllib3
+
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
 
 def writeToJson(f):
 
